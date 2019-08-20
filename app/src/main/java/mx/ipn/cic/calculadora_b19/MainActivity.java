ae58d9444b9a5faa58cf09f5c8ff5c91b01a091b
Eduardo Mies <eduardo.mireles.21@gmail.c>
2019-08-20 10:45:52 -0500

    Initial commit

---
+++ app/src/main/java/mx/ipn/cic/calculadora_b19/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.ipn.cic.calculadora_b19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
