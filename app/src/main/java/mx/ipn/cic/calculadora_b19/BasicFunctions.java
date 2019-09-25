76fc3ddd1b4540bd39846ccd84f0613e28e10bd4
silvia <anasilvia_mh@hotmail.com>
2019-09-25 18:29:32 -0500

    Se agregaron fragmentos.

---
+++ app/src/main/java/mx/ipn/cic/calculadora_b19/BasicFunctions.java
@@ -0,0 +1,24 @@
+package mx.ipn.cic.calculadora_b19;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BasicFunctions extends Fragment {
+
+    public BasicFunctions() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_basic_functions, container, false);
+    }
+}
---
+++ app/src/main/java/mx/ipn/cic/calculadora_b19/ScientificFunctions.java
@@ -0,0 +1,24 @@
+package mx.ipn.cic.calculadora_b19;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ScientificFunctions extends Fragment {
+
+    public ScientificFunctions() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scientific_functions, container, false);
+    }
+}
